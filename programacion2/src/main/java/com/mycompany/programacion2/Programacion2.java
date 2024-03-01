993fc418c04f369b44204a587539fb89d41d5202
HeidyCamargo <hpcamargoa@ufpso.edu.co>
2024-03-01 11:39:54 -0500

    creacion de la clase interna

--- programacion2/src/main/java/com/mycompany/programacion2/Programacion2.java
+++ programacion2/src/main/java/com/mycompany/programacion2/Programacion2.java
@@ -4,13 +4,28 @@
 
 package com.mycompany.programacion2;
 
-/**
- *
- * @author SCIS2PC24
- */
 public class Programacion2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Character homero = new Character("homero", "inspector de seguridad", 33, "pequeño demonio");
+        homero.greeting(); // Llamada al método greeting()
+    }
+
+    static class Character {
+        String name;
+        String occupation;
+        int age;
+        String phrase;
+
+        public Character(String name, String occupation, int age, String phrase) {
+            this.name = name;
+            this.occupation = occupation;
+            this.age = age;
+            this.phrase = phrase;
+        }
+
+        void greeting() {
+            System.out.println(this.phrase);
+        }
     }
 }
